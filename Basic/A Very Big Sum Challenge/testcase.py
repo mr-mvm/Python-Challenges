30579f3a454f4e1f71ffa0bae123b89ecd4dfe59
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-07 23:24:20 +0530

    Update testcase.py

--- Basic/A Very Big Sum Challenge/testcase.py
+++ Basic/A Very Big Sum Challenge/testcase.py
@@ -2,19 +2,20 @@ import unittest
 from solution import aVeryBigSum
 
 class TestAVeryBigSum(unittest.TestCase):
-    def test_small_numbers(self):
-        self.assertEqual(aVeryBigSum([1, 2, 3, 4, 5]), 15)
-        self.assertEqual(aVeryBigSum([10, 20, 30]), 60)
+    def test_standard_case(self):
+        self.assertEqual(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]), 5000000015)
+
+    def test_single_element(self):
+        self.assertEqual(aVeryBigSum([123456789]), 123456789)
+
+    def test_all_zeros(self):
+        self.assertEqual(aVeryBigSum([0, 0, 0, 0, 0]), 0)
 
     def test_large_numbers(self):
-        self.assertEqual(aVeryBigSum([1000000000, 2000000000, 3000000000]), 6000000000)
-        self.assertEqual(aVeryBigSum([9223372036854775807, 1]), 9223372036854775808)
-
-    def test_empty_list(self):
-        self.assertEqual(aVeryBigSum([]), 0)
+        self.assertEqual(aVeryBigSum([999999999, 999999999, 999999999]), 2999999997)
 
     def test_mixed_numbers(self):
-        self.assertEqual(aVeryBigSum([10, -5, 15, -5]), 15)
+        self.assertEqual(aVeryBigSum([-1, 0, 1, 2, -2]), 0)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
