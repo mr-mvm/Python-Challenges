bc4a638aafc0eec97951682ff2ce0276e0c57c78
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-07 23:12:15 +0530

    Update testcase.py

--- Basic/Birthday Cake Candles Challenge/testcase.py
+++ Basic/Birthday Cake Candles Challenge/testcase.py
@@ -6,16 +6,16 @@ class TestBirthdayCakeCandles(unittest.TestCase):
         self.assertEqual(birthdayCakeCandles([4, 4, 1, 3]), 2)
 
     def test_all_same_height(self):
-        self.assertEqual(birthdayCakeCandles([2, 2, 2, 2]), 4)
+        self.assertEqual(birthdayCakeCandles([3, 3, 3, 3]), 4)
 
-    def test_single_candle(self):
-        self.assertEqual(birthdayCakeCandles([7]), 1)
-
-    def test_mixed_heights(self):
-        self.assertEqual(birthdayCakeCandles([3, 2, 1, 3, 4, 4]), 2)
+    def test_one_tallest_candle(self):
+        self.assertEqual(birthdayCakeCandles([1, 2, 3, 4, 5]), 1)
 
     def test_large_numbers(self):
-        self.assertEqual(birthdayCakeCandles([1000000000, 999999999, 1000000000, 1000000000]), 3)
+        self.assertEqual(birthdayCakeCandles([1000000, 1000000, 999999]), 2)
 
-if __name__ == "__main__":
+    def test_single_candle(self):
+        self.assertEqual(birthdayCakeCandles([1]), 1)
+
+if __name__ == '__main__':
     unittest.main()
