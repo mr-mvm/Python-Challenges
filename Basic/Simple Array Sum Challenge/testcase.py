c7d6d34deef8d6cda7405cefa5ba6faf124e5039
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-07 23:21:47 +0530

    Update testcase.py

--- Basic/Simple Array Sum Challenge/testcase.py
+++ Basic/Simple Array Sum Challenge/testcase.py
@@ -2,23 +2,20 @@ import unittest
 from solution import simpleArraySum
 
 class TestSimpleArraySum(unittest.TestCase):
-    def test_positive_numbers(self):
-        self.assertEqual(simpleArraySum([1, 2, 3, 4, 5]), 15)
-        self.assertEqual(simpleArraySum([10, 20, 30]), 60)
-
-    def test_negative_numbers(self):
-        self.assertEqual(simpleArraySum([-1, -2, -3]), -6)
-        self.assertEqual(simpleArraySum([-10, -20, 30]), 0)
-
-    def test_mixed_numbers(self):
-        self.assertEqual(simpleArraySum([0, 0, 0]), 0)
-        self.assertEqual(simpleArraySum([10, -5, 15]), 20)
+    def test_standard_case(self):
+        self.assertEqual(simpleArraySum([1, 2, 3, 4, 10, 11]), 31)
 
     def test_empty_array(self):
         self.assertEqual(simpleArraySum([]), 0)
 
-    def test_large_numbers(self):
-        self.assertEqual(simpleArraySum([1000000, 2000000, 3000000]), 6000000)
+    def test_single_element(self):
+        self.assertEqual(simpleArraySum([5]), 5)
 
-if __name__ == "__main__":
+    def test_negative_numbers(self):
+        self.assertEqual(simpleArraySum([-1, -2, -3, -4, -5]), -15)
+
+    def test_large_numbers(self):
+        self.assertEqual(simpleArraySum([1000000, 1000000, 1000000]), 3000000)
+
+if __name__ == '__main__':
     unittest.main()
