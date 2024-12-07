671685f3601b6de60651d5c8f73e05de517ba028
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-07 23:19:03 +0530

    Update testcase.py

--- Basic/Simple Sum Challenge/testcase.py
+++ Basic/Simple Sum Challenge/testcase.py
@@ -3,20 +3,19 @@ from solution import solveMeFirst
 
 class TestSolveMeFirst(unittest.TestCase):
     def test_positive_numbers(self):
-        self.assertEqual(solveMeFirst(3, 7), 10)
-        self.assertEqual(solveMeFirst(100, 200), 300)
+        self.assertEqual(solveMeFirst(5, 7), 12)
 
     def test_negative_numbers(self):
-        self.assertEqual(solveMeFirst(-5, -10), -15)
-        self.assertEqual(solveMeFirst(-1, 1), 0)
+        self.assertEqual(solveMeFirst(-3, -6), -9)
+
+    def test_mixed_sign_numbers(self):
+        self.assertEqual(solveMeFirst(4, -2), 2)
 
     def test_zero(self):
         self.assertEqual(solveMeFirst(0, 0), 0)
-        self.assertEqual(solveMeFirst(0, 5), 5)
 
     def test_large_numbers(self):
-        self.assertEqual(solveMeFirst(123456, 654321), 777777)
+        self.assertEqual(solveMeFirst(1000000, 2000000), 3000000)
 
 if __name__ == "__main__":
-    # Run all test cases
     unittest.main()
