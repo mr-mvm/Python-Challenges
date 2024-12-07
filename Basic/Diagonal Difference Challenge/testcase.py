7aba25f7cfa1d411b67967c8024193525019b0ab
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-07 23:26:05 +0530

    Update testcase.py

--- Basic/Diagonal Difference Challenge/testcase.py
+++ Basic/Diagonal Difference Challenge/testcase.py
@@ -2,22 +2,28 @@ import unittest
 from solution import diagonalDifference
 
 class TestDiagonalDifference(unittest.TestCase):
-    def test_small_matrix(self):
-        self.assertEqual(diagonalDifference([[1, 2], [3, 4]]), 0)
-        self.assertEqual(diagonalDifference([[5, 1], [2, 5]]), 7)
-
-    def test_large_matrix(self):
-        self.assertEqual(diagonalDifference([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 0)
+    def test_standard_case(self):
         self.assertEqual(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]), 15)
 
+    def test_zero_matrix(self):
+        self.assertEqual(diagonalDifference([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), 0)
+
     def test_single_element(self):
-        self.assertEqual(diagonalDifference([[42]]), 0)
+        self.assertEqual(diagonalDifference([[5]]), 0)
 
     def test_negative_numbers(self):
         self.assertEqual(diagonalDifference([[-1, -2, -3], [-4, -5, -6], [-7, -8, -9]]), 0)
 
-    def test_mixed_numbers(self):
-        self.assertEqual(diagonalDifference([[3, 2, 1], [6, 5, 4], [9, 8, 7]]), 0)
+    def test_large_matrix(self):
+        self.assertEqual(
+            diagonalDifference([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+                [13, 14, 15, 16]
+            ]),
+            0
+        )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
