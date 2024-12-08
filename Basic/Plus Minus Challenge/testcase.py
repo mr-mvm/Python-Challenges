659fb1b9b4c6ba14651847179108e40c490c2291
Mrunaal Varma M <159999317+mr-mvm@users.noreply.github.com>
2024-12-08 19:00:48 +0530

    Update testcase.py

--- Basic/Plus Minus Challenge/testcase.py
+++ Basic/Plus Minus Challenge/testcase.py
@@ -12,27 +12,27 @@ class TestPlusMinus(unittest.TestCase):
         sys.stdout = captured_output = StringIO()
         func(*args)
         sys.stdout = old_stdout
-        return captured_output.getvalue().strip()
+        return captured_output.getvalue().strip().split('\n')
+
+    def test_standard_case(self):
+        result = self.capture_output(plusMinus, [-4, 3, -9, 0, 4, 1])
+        self.assertEqual(result, ["0.500000", "0.333333", "0.166667"])
 
     def test_all_positive(self):
         result = self.capture_output(plusMinus, [1, 2, 3, 4, 5])
-        self.assertEqual(result, "1.000000\n0.000000\n0.000000")
+        self.assertEqual(result, ["1.000000", "0.000000", "0.000000"])
 
     def test_all_negative(self):
-        result = self.capture_output(plusMinus, [-1, -2, -3])
-        self.assertEqual(result, "0.000000\n1.000000\n0.000000")
+        result = self.capture_output(plusMinus, [-1, -2, -3, -4, -5])
+        self.assertEqual(result, ["0.000000", "1.000000", "0.000000"])
 
-    def test_all_zero(self):
-        result = self.capture_output(plusMinus, [0, 0, 0])
-        self.assertEqual(result, "0.000000\n0.000000\n1.000000")
+    def test_with_zeros(self):
+        result = self.capture_output(plusMinus, [0, 0, 0, -1, 1])
+        self.assertEqual(result, ["0.200000", "0.200000", "0.600000"])
 
-    def test_mixed(self):
-        result = self.capture_output(plusMinus, [-4, 3, -9, 0, 4, 1])
-        self.assertEqual(result, "0.500000\n0.333333\n0.166667")
+    def test_empty_array(self):
+        result = self.capture_output(plusMinus, [])
+        self.assertEqual(result, [])
 
-    def test_single_element(self):
-        result = self.capture_output(plusMinus, [0])
-        self.assertEqual(result, "0.000000\n0.000000\n1.000000")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
